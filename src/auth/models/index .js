4aade931e5bbcf643465bdd9433d0cb43012084c
Lacey Washington <lacey.m.washington@gmail.com>
2023-07-22 17:51:44 -0500

    test

---
+++ src/auth/models/index .js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const userSchema = require('./users.model.js');
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory' : process.env.DATABASE_URL;
+
+const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
+  dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false,
+  //   }
+  }
+} : {};
+
+const db = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+console.log('I am the DataBase', db);
+const usersModel = userSchema(db, DataTypes);
+
+module.exports = {
+  db,
+  usersModel
+};
